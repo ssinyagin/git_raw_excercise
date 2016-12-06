7e7cd18acc8db938e1cc5aa19995299fdfc013c4
Stanislav Sinyagin <ssinyagin@k-open.com>
2016-12-06 10:08:52 +0100

    bulk_perf_test.pl: refactor

--- bulk_perf_test.pl
+++ bulk_perf_test.pl
@@ -26,10 +26,6 @@ foreach my $dir ($repodir1, $repodir2)
     remove_tree($dir);
 }
 
-sub _signature
-{
-    return Git::Raw::Signature->now('Z', 'x@x.com');
-}
 
 my $updated_files = {};
 
@@ -49,22 +45,10 @@ my $checkout_opts = {
     'callbacks' => {'progress' => $checkout_progress_cb},
 };
 
-sub _print_updates
-{
-    printf("Total %d updates\n", scalar(keys %{$updated_files}));
-}
 
 
 my $ts_start = time();
 my $prev_ts = $ts_start;
-
-sub _print_time
-{
-    my $msg = shift;
-    my $now = time();
-    printf("%f (%f) %s\n", $now - $ts_start, $now - $prev_ts, $msg);
-    $prev_ts = $now;
-}
     
 
 ## Producer repo
@@ -109,71 +93,117 @@ my $r2 = Git::Raw::Repository->init($repodir2, 0);
 
 _print_time('Created r2 repo');
 
-
-
-my $data = {
-    'docsIfDownstreamChannelTable' => '1.3.6.1.2.1.10.127.1.1.1',
-    'docsIfCmtsDownChannelCounterTable' => '1.3.6.1.2.1.10.127.1.3.10',
-    'docsIfSigQSignalNoise' => '1.3.6.1.2.1.10.127.1.1.4.1.5',
-    'ciscoLS1010'                       => '1.3.6.1.4.1.9.1.107',
-    'ciscoImageTable'                   => '1.3.6.1.4.1.9.9.25.1.1',
-    'ceImageTable'                      => '1.3.6.1.4.1.9.9.249.1.1.1',
-    'bufferElFree'                      => '1.3.6.1.4.1.9.2.1.9.0',
-    'cipSecGlobalHcInOctets'            => '1.3.6.1.4.1.9.9.171.1.3.1.4.0',
-    'cbgpPeerAddrFamilyName'            => '1.3.6.1.4.1.9.9.187.1.2.3.1.3',
-    'cbgpPeerAcceptedPrefixes'          => '1.3.6.1.4.1.9.9.187.1.2.4.1.1',
-    'cbgpPeerPrefixAdminLimit'          => '1.3.6.1.4.1.9.9.187.1.2.4.1.3',
-    'ccarConfigType'                    => '1.3.6.1.4.1.9.9.113.1.1.1.1.3',
-    'ccarConfigAccIdx'                  => '1.3.6.1.4.1.9.9.113.1.1.1.1.4',
-    'ccarConfigRate'                    => '1.3.6.1.4.1.9.9.113.1.1.1.1.5',
-    'ccarConfigLimit'                   => '1.3.6.1.4.1.9.9.113.1.1.1.1.6',
-    'ccarConfigExtLimit'                => '1.3.6.1.4.1.9.9.113.1.1.1.1.7',
-    'ccarConfigConformAction'           => '1.3.6.1.4.1.9.9.113.1.1.1.1.8',
-    'ccarConfigExceedAction'            => '1.3.6.1.4.1.9.9.113.1.1.1.1.9',
-    'cvpdnSystemTunnelTotal'            => '1.3.6.1.4.1.9.10.24.1.1.4.1.2',
-    'c3gStandard'                       => '1.3.6.1.4.1.9.9.661.1.1.1.1',
-    'cportQosDropPkts'                  => '1.3.6.1.4.1.9.9.189.1.3.2.1.7',
-};
-
-my $json = JSON->new;
-
-while( $count-- > 0 )
-{
-    my $sha = sha1_hex(' ' . $count . ' ');
-
-    my $dir = $repodir1 . '/' . substr($sha, 0, 2) . '/' .
-        substr($sha, 2, 2) . '/';
-
-    if( not -d $dir )
-    {
-        make_path($dir) or die("Cannot mkdir $dir: $!");
-    }
-    
-    my $filepath = $dir . $sha;
-       
-    my $fh = IO::File->new($filepath, 'w') or die("Cannot open $filepath: $!");
-    $fh->print($json->encode($data));
-    $fh->close;
-}
-
-_print_time('Wrote JSON data');
+_gen_data_r1($count, 0);
 
 # produce one large commit
+_commit_all_r1('First large commit');
+_pull_r2();
+_read_data($repodir2, $updated_files);
+
+# Overwrite the same files wioth the same data
+_gen_data_r1($count, 0);
+
+_commit_all_r1('This should be empty commit');
+_pull_r2();
+_read_data($repodir2, $updated_files);
+
+
+
+sub _signature
 {
+    return Git::Raw::Signature->now('Z', 'x@x.com');
+}
+
+sub _print_updates
+{
+    printf("Total %d updates\n", scalar(keys %{$updated_files}));
+}
+
+
+sub _print_time
+{
+    my $msg = shift;
+    my $now = time();
+    printf("%f (%f) %s\n", $now - $ts_start, $now - $prev_ts, $msg);
+    $prev_ts = $now;
+}
+
+
+sub _gen_data_r1
+{
+    my $n_nodes = shift;
+    my $n_start = shift;
+
+    my $data = {
+        'docsIfDownstreamChannelTable' => '1.3.6.1.2.1.10.127.1.1.1',
+        'docsIfCmtsDownChannelCounterTable' => '1.3.6.1.2.1.10.127.1.3.10',
+        'docsIfSigQSignalNoise' => '1.3.6.1.2.1.10.127.1.1.4.1.5',
+        'ciscoLS1010'                       => '1.3.6.1.4.1.9.1.107',
+        'ciscoImageTable'                   => '1.3.6.1.4.1.9.9.25.1.1',
+        'ceImageTable'                      => '1.3.6.1.4.1.9.9.249.1.1.1',
+        'bufferElFree'                      => '1.3.6.1.4.1.9.2.1.9.0',
+        'cipSecGlobalHcInOctets'            => '1.3.6.1.4.1.9.9.171.1.3.1.4.0',
+        'cbgpPeerAddrFamilyName'            => '1.3.6.1.4.1.9.9.187.1.2.3.1.3',
+        'cbgpPeerAcceptedPrefixes'          => '1.3.6.1.4.1.9.9.187.1.2.4.1.1',
+        'cbgpPeerPrefixAdminLimit'          => '1.3.6.1.4.1.9.9.187.1.2.4.1.3',
+        'ccarConfigType'                    => '1.3.6.1.4.1.9.9.113.1.1.1.1.3',
+        'ccarConfigAccIdx'                  => '1.3.6.1.4.1.9.9.113.1.1.1.1.4',
+        'ccarConfigRate'                    => '1.3.6.1.4.1.9.9.113.1.1.1.1.5',
+        'ccarConfigLimit'                   => '1.3.6.1.4.1.9.9.113.1.1.1.1.6',
+        'ccarConfigExtLimit'                => '1.3.6.1.4.1.9.9.113.1.1.1.1.7',
+        'ccarConfigConformAction'           => '1.3.6.1.4.1.9.9.113.1.1.1.1.8',
+        'ccarConfigExceedAction'            => '1.3.6.1.4.1.9.9.113.1.1.1.1.9',
+        'cvpdnSystemTunnelTotal'            => '1.3.6.1.4.1.9.10.24.1.1.4.1.2',
+        'c3gStandard'                       => '1.3.6.1.4.1.9.9.661.1.1.1.1',
+        'cportQosDropPkts'                  => '1.3.6.1.4.1.9.9.189.1.3.2.1.7',
+    };
+
+    my $json = JSON->new;
+    $json->canonical(1);
+    
+    my $cnt = 0;
+    while( $cnt++ < $n_nodes )
+    {
+        my $sha = sha1_hex(' ' . ($cnt+$n_start) . ' ');
+
+        my $dir = $repodir1 . '/' . substr($sha, 0, 2) . '/' .
+            substr($sha, 2, 2) . '/';
+        
+        if( not -d $dir )
+        {
+            make_path($dir) or die("Cannot mkdir $dir: $!");
+        }
+        
+        my $filepath = $dir . $sha;
+        
+        my $fh = IO::File->new($filepath, 'w')
+            or die("Cannot open $filepath: $!");
+        $fh->print($json->encode($data));
+        $fh->close;
+    }
+    
+    _print_time(sprintf('Wrote JSON data to r1: %d nodes, starting from %d',
+                        $n_nodes, $n_start));
+}
+
+
+
+sub _commit_all_r1
+{
+    my $msg = shift;
+    
     my $index = $r1->index;
     $index->add_all({});
     $index->write;
     my $tree = $index->write_tree();
     my $me = _signature();
     my $head = $r1->head->target;
-    $r1->commit('Big data commit', $me, $me, [$head], $tree);
+    $r1->commit($msg, $me, $me, [$head], $tree);
+    _print_time('Commit: ' . $msg);
 }
 
 
-_print_time('Made a large commit');
-
-
-# pull it into r2
+sub _pull_r2
 {
     my $branch = Git::Raw::Branch->lookup($r2, 'DataTree', 1);    
     die("Cannot lookup branch") unless defined($branch);
@@ -182,7 +212,7 @@ _print_time('Made a large commit');
     die('X3') unless defined($remote);
     
     $remote->fetch();
-    _print_time('Fetch the large commit');    
+    _print_time('Fetch');    
 
     my $ref = Git::Raw::Reference->lookup('remotes/origin/DataTree', $r2 );
     die('REF3') unless defined($ref);
@@ -191,63 +221,55 @@ _print_time('Made a large commit');
     die("Only supports fast-forward merges at this time")
         if (!grep {$_ eq 'fast_forward'} @$analysis);
 
-    _print_time('merge_analysis');    
-    
     my $commit = $ref->peel('commit');
     my $tree = $commit->tree();
     my $index = $r2->index();
     
     $index->read_tree($tree);
-    _print_time('index->read_tree');    
-
     $index->write();
-    _print_time('index->write');    
-
     $r2->checkout($tree, $checkout_opts);
-    _print_time('checkout');    
     $r2->head($branch->target($commit));
-    _print_time('move head');    
-    
+
+    _print_time('Checkout');        
     _print_updates();
 }
 
-# read JSON data
+    
 
-my $d0 = IO::Dir->new($repodir2) or die($!);
 
-while( defined(my $dir0 = $d0->read()) )
+sub _read_data
 {
-    if( $dir0 =~ /^\w{2}$/ )
+    my $dir = shift;
+    my $files = shift;
+
+    my $count_read = 0;
+    my $count_deleted = 0;
+    my $json = JSON->new;
+    
+    while( my ($fname, $dummy) = each %{$files} )
     {
-        my $path1 = $repodir2 . '/' . $dir0;
-        my $d1 = IO::Dir->new($path1) or die($!);
-
-        while( defined(my $dir1 = $d1->read()) )
+        my $filename = $dir . '/' . $fname;
+        if( -e $filename )
         {
-            if( $dir1 =~ /^\w{2}$/ )
-            {
-                my $path2 = $path1 . '/' . $dir1;
-                my $d2 = IO::Dir->new($path2) or die($!);
+            local $/;
+            my $fh = IO::File->new($filename)
+                or die("Cannot open $filename: $!");
 
-                while( defined(my $fname = $d2->read()) )
-                {
-                    if( $fname =~ /\w/ )
-                    {
-                        local $/;
-                        my $filepath = $path2 . '/' . $fname;
-                        my $fh = IO::File->new($filepath)
-                            or die("Cannot open $filepath: $!");
-
-                        my $data = $json->decode($fh->getline);
-                        die("empty data") unless defined($data);
-                        die("not a hash") unless ref($data) eq 'HASH';
-                        
-                        $fh->close;
-                    }
-                }
-            }
+            my $data = $json->decode($fh->getline);
+            die("empty data") unless defined($data);
+            die("not a hash") unless ref($data) eq 'HASH';
+            
+            $fh->close;
+            $count_read++;
+        }
+        else
+        {
+            $count_deleted++;
         }
     }
+
+    _print_time(sprintf('Read %d files, deleted %d files',
+                        $count_read, $count_deleted));    
 }
 
-_print_time('Read JSON');    
+
