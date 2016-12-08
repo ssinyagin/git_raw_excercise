fe0aa071a0f1f0a3c2f8f3b29219f35e620d3299
Jacques Germishuys <jacquesg@striata.com>
2016-12-08 19:27:21 +0200

    Use the index as a treebuilder

--- treebuilder_example.pl
+++ treebuilder_example.pl
@@ -16,46 +16,39 @@ my $repodir1 = $homedir . '/r1';
 remove_tree($repodir1);
 
 my $r1 = Git::Raw::Repository->init($repodir1, 1);
+my $index = $r1->index();
 
-my $builder = Git::Raw::Tree::Builder->new($r1);
-
-my $blob = $r1->blob("Hello world\n");
-
-my $entry = $builder->insert('hello_world.txt', $blob, 0100644);
-
-my $tree = $builder->write();
+# Initial
+$index->add_frombuffer('hello_world.txt', "Hello world\n");
+my $tree = $index->write_tree();
+print "Created initial tree: $tree\n";
 
 my $me = _signature();
-$r1->commit('Hello World commit', $me, $me, [], $tree);
+my $commit1 = $r1->commit('Hello World commit', $me, $me, [], $tree);
+print "Created commit1: $commit1\n";
 
 my $branch = Git::Raw::Branch->lookup( $r1, 'master', 1 );
 $branch->move('DataTree', 1);
 
+#Optional (needed if the commit has changed)
+$index->read_tree ($commit1->tree());
 
+$index->add_frombuffer('c.txt', 'somecontents2');
+$index->add_frombuffer('b/c.txt', 'somecontents2');
+$index->add_frombuffer('a/b/c.txt', 'somecontents2');
+$index->add_frombuffer('b.txt', 'somecontents1');
+$index->add_frombuffer('c.txt', 'somecontents2');
+$index->add_frombuffer('a.txt', 'somecontents3');
+$index->add_frombuffer('d.txt', 'somecontents4');
 
-sub_add_commit
-{
-    my $data = shift; # hash filename->content
-    my $msg = shift;  # commit message
+$tree = $index->write_tree();
 
-    foreach my $fname (sort keys %{$data})
-    {
-        my @path_elements = split('/', $fname);
+print "Created new tree: $tree\n";
+my $commit2 = $r1->commit('Hello World commit', $me, $me, [$commit1], $tree);
+print "Created commit2: $commit2\n";
 
-        # need some recursive majic here
-        
-    }
-}
-
-
-
-
-
-
-
-
-
-
+print "Listing: git ls-tree -r $tree\n";
+system ("cd $repodir1; git ls-tree -r $tree");
 
 sub _signature
 {
